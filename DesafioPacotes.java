4a2fd6bf17792eec4095c47d2f2cab08de22020c
Snaymi <sneymeb@hotmail.com>
2023-03-27 19:27:11 -0300

    Finalizado

---
+++ DesafioPacotes.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class DesafioPacotes {
+    public static  void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+        int leitor;
+        leitor = scan.nextInt();
+
+        if (leitor == 1) {
+            System.out.println("/\n");
+        }
+        if (leitor == 2) {
+            System.out.println("//\n");
+        }
+        if (leitor == 3) {
+            System.out.println("///\n");
+        }
+        if (leitor == 4) {
+            System.out.println("////\n");
+        }
+        if (leitor == 5) {
+            System.out.println("/////\n");
+        }
+        if (leitor == 6) {
+            System.out.println("//////\n");
+        }
+        if (leitor == 7) {
+            System.out.println("///////\n");
+        }
+        if (leitor == 8) {
+            System.out.println("////////\n");
+        }
+        if (leitor == 9) {
+            System.out.println("/////////\n");
+        }
+        if (leitor == 10) {
+            System.out.println("//////////\n");
+        }
+
+    }
+}
